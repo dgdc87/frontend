2ff25ecf7eb6924f2642deb6336b0f00bbd2bafb
Diego <dgdelcampo@outlook.es>
2020-05-11 18:01:45 +0200

    versión 1.0.0

--- src/app/components/perfil/perfil.component.ts
+++ src/app/components/perfil/perfil.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '@services/authentication/authentication.service';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
-import { DialogComponent } from '@dgdc87/dialog';
 
 @Component({
   selector: 'app-perfil',
